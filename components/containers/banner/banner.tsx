6568aeda66fdebbe3f52fd27df3de52358ae172a
Spoonman <155550137+JJKnegtel@users.noreply.github.com>
2024-11-13 10:01:35 +0000

    Update ' to &apos; because ' sucks and breaks things. Coding is hard :(

--- components/containers/banner/banner.tsx
+++ components/containers/banner/banner.tsx
@@ -2,7 +2,7 @@ export const Banner = () => {
   return (
     <section className="flex justify-center bg-foreground p-2">
       <span className="text-center text-sm font-light text-muted/70">
-        NOW IN BETA: Experience The Grid's validated Solana data layer
+        NOW IN BETA: Experience The Grid&apos;s validated Solana data layer
       </span>
     </section>
   );
